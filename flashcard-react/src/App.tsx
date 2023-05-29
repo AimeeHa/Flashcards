f336da1bbf9222eca5016e5c0d8e16b7ae213bf8
Aimee Ha <aimee.ha95@gmail.com>
2023-05-29 18:52:15 +0100

    changed homepage's random term display - overrode with inline style, class didn't work somehow

--- flashcard-react/src/App.tsx
+++ flashcard-react/src/App.tsx
@@ -17,32 +17,52 @@ function App() {
         <div className="articles">
           <div className="articles one">
             <div className="article-header">RANDOM TERM OF THE DAY</div>
-            <div className="main-message"></div>
-            <div className="view-definition">
-              <a href="">What's this?</a>
-              <div className="view-arrow">
-                <EastRoundedIcon style={{ width: '0.6em' }} />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                flexGrow: 0.8,
+                gap: '12px',
+              }}
+            >
+              <div className="terms">{/* TODO: */} AAAAA</div>
+              <div className="view-definition">
+                <a href="">What's this?</a>
+                <div className="view-arrow">
+                  <EastRoundedIcon style={{ width: '0.6em' }} />
+                </div>
               </div>
             </div>
           </div>
           <div className="articles two">
             <div className="article-header">RANDOM TERM OF THE DAY</div>
-            <div className="main-message"></div>
-            <div className="view-definition">
-              <a href="">What's this?</a>
-              <div className="view-arrow">
-                <EastRoundedIcon style={{ width: '0.6em' }} />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                flexGrow: 0.8,
+                gap: '12px',
+              }}
+            >
+              <div className="terms">{/* TODO: */} BBBBB</div>
+              <div className="view-definition">
+                <a href="">What's this?</a>
+                <div className="view-arrow">
+                  <EastRoundedIcon style={{ width: '0.6em' }} />
+                </div>
               </div>
             </div>
           </div>
           <div className="articles three">
             <div className="article-header">CONTACT US</div>
-            <div className="main-message">
+            <div className="contact-message">
               If you have encountered any issues while using Aimee's Cards, or
               have any suggestions for improving the application, don't hesitate
               to reach out!
             </div>
-            <div className="smaller-note">
+            <div className="contact-note">
               (Or even if you just want to share a random story &#128521;)
             </div>
             <div className="leave-message">
