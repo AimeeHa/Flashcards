ca2ea543d868d6f852b7afac14fa0f7bc51397d0
Aimee Ha <aimee.ha95@gmail.com>
2023-06-02 23:58:41 +0100

    started model.py with user model

--- flashcard2/models.py
+++ flashcard2/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        
