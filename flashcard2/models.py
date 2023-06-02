f6bc06fd7ae1fc83f70c103bd971f230422eb20d
Aimee Ha <aimee.ha95@gmail.com>
2023-06-03 00:10:56 +0100

    category. flashcard & set classes added, return values to be updated

--- flashcard2/models.py
+++ flashcard2/models.py
@@ -1,9 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
 
-class User:
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
-        
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class FlashcardSet(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="flashcard_sets")
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="flashcard_sets")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Flashcard(models.Model):
+    id = models.AutoField(primary_key=True)
+    set_id = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, related_name="flashcards")
+    term = models.CharField(max_length=255)
+    definition = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Flashcard {self.id} - {self.term}"
