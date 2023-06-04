3632106a5be92626fe7c94555978121d0d5d25a0
Aimee Ha <aimee.ha95@gmail.com>
2023-06-04 21:34:42 +0100

    added category choices

--- flashcard2/models.py
+++ flashcard2/models.py
@@ -5,8 +5,13 @@ class User(AbstractUser):
     pass
 
 class Category(models.Model):
+    CATEGORIES = [
+        ("English", "English"),
+        ("Technology", "Technology"),
+        ("Others", "Others")
+    ]
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, choices=CATEGORIES, default="Others")
 
     def __str__(self):
         return f"{self.name}"
@@ -20,15 +25,15 @@ class FlashcardSet(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} by {self.user.username}"
 
 class Flashcard(models.Model):
     id = models.AutoField(primary_key=True)
-    set_id = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, related_name="flashcards")
+    set = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, related_name="flashcards")
     term = models.CharField(max_length=255)
     definition = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True).strftime("%b %-d %Y, %-I:%M %p")
+    updated_at = models.DateTimeField(auto_now=True).strftime("%b %-d %Y, %-I:%M %p")
 
     def __str__(self):
-        return f"Flashcard {self.id} - {self.term}"
+        return f"Flashcard {self.set.name} - {self.term}"
