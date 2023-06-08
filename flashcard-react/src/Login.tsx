e4404e236498b48fb4657bbb120f43871ca48447
Aimee Ha <aimee.ha95@gmail.com>
2023-06-08 17:42:58 +0100

    login design done, need a background photo for left side

--- flashcard-react/src/Login.tsx
+++ flashcard-react/src/Login.tsx
@@ -18,19 +18,32 @@ function Login() {
         </div>
         <div className="right">
           <form className="login-form">
-            <div className="login-title">Log In</div>
             <div className="username">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
+              <input
+                type="text"
+                id="username"
+                placeholder="Type your username"
+              />
             </div>
             <div className="password">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input
+                type="password"
+                id="password"
+                placeholder="Type your password"
+              />
             </div>
             <div className="login-button">
               <button className="blue-button">Log In</button>
+              <div className="link-to-others">
+                <a href="/resetpassword">Forgot your password?</a>
+              </div>
             </div>
           </form>
+          <div className="link-to-others">
+            <a href="/register">Don't have an account? Register here.</a>
+          </div>
         </div>
       </div>
     </>
