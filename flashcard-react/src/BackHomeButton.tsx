94db32d13c960e65732d2ebccb52dbf978451f0e
Aimee Ha <aimee.ha95@gmail.com>
2023-06-19 18:08:17 +0100

    added useNavigate for back home button as well

--- flashcard-react/src/BackHomeButton.tsx
+++ flashcard-react/src/BackHomeButton.tsx
@@ -1,13 +1,20 @@
 import WestRoundedIcon from '@mui/icons-material/WestRounded';
 import './BackHomeButton.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function BackHomeButton() {
+  const navigate = useNavigate();
   return (
-    <div className="back-to-home">
+    <div
+      className="back-to-home"
+      onClick={() => {
+        navigate('/');
+      }}
+    >
       <div id="back-arrow">
         <WestRoundedIcon style={{ width: '0.7em' }} />
       </div>
-      <a href="/">Back to Home</a>
+      <div>Back to Home</div>
     </div>
   );
 }
