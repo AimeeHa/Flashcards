8d09d65a55e310aa76124fe2d8d2c4aad7cdf3d9
Aimee Ha <aimee.ha95@gmail.com>
2023-06-23 20:16:45 +0100

    added error message div to register - having 500 error, not sure why

--- flashcard-react/src/Register.tsx
+++ flashcard-react/src/Register.tsx
@@ -2,42 +2,89 @@ import './Register.css';
 import './Button.css';
 import InputRow from './InputRow';
 import BackHomeButton from './BackHomeButton';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
+  const navigate = useNavigate();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmation, setConfirmation] = useState('');
 
-  const handleNameChange = async (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setName(event.target.value);
+  const [isPwError, setIsPwError] = useState(false);
+  const [isEmailError, setIsEmailError] = useState(false);
+
+  const createHandleInputChange = (setStateFunc: any) => {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      setStateFunc(event.target.value);
+    };
   };
 
-  const handleEmailChange = async (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setEmail(event.target.value);
-  };
+  const handleNameChange = createHandleInputChange(setName);
+  const handleEmailChange = createHandleInputChange(setEmail);
+  const handlePasswordChange = createHandleInputChange(setPassword);
+  const handleConfirmationChange = createHandleInputChange(setConfirmation);
 
-  const handlePasswordChange = async (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setPassword(event.target.value);
-  };
+  // const handleNameChange = async (
+  //   event: React.ChangeEvent<HTMLInputElement>,
+  // ) => {
+  //   setName(event.target.value);
+  // };
 
-  const handleConfirmationChange = async (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setConfirmation(event.target.value);
-  };
+  // const handleEmailChange = async (
+  //   event: React.ChangeEvent<HTMLInputElement>,
+  // ) => {
+  //   setEmail(event.target.value);
+  // };
+
+  // const handlePasswordChange = async (
+  //   event: React.ChangeEvent<HTMLInputElement>,
+  // ) => {
+  //   setPassword(event.target.value);
+  // };
+
+  // const handleConfirmationChange = async (
+  //   event: React.ChangeEvent<HTMLInputElement>,
+  // ) => {
+  //   setConfirmation(event.target.value);
+  // };
+
+  useEffect(() => {
+    let timeoutId: number | undefined;
+
+    if (isPwError) {
+      timeoutId = setTimeout(() => {
+        setIsPwError(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+    [isPwError];
+  });
+
+  useEffect(() => {
+    let timeoutId: number | undefined;
+
+    if (isEmailError) {
+      timeoutId = setTimeout(() => {
+        setIsEmailError(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+    [isEmailError];
+  });
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (password !== confirmation) {
-      alert('Passwords do not match!'); //TODO: change to a better way
+      setIsPwError(true);
     }
 
     const formData = { name: name, email: email, password: password };
@@ -53,8 +100,11 @@ function Register() {
 
     if (response.status === 201) {
       console.log('Successfully registered!');
+      navigate('/login');
+    } else if (response.status === 400) {
+      setIsEmailError(true);
     } else {
-      alert('This email has already been registered!'); //TODO: change to a better way
+      alert(response.status + ' ' + response.statusText); //TO CHANGE
     }
   };
 
@@ -101,10 +151,23 @@ function Register() {
               </button>
             </div>
           </form>
-          <div>Passwords do not match. Please re-enter your passwords.</div>
-          <div>
-            This email has already been registered. Please use another email or
-            back to <a href="/login">login</a>.
+          <div className={isPwError ? 'alert-shown' : 'alert-hidden'}>
+            Passwords do not match. Please re-enter your passwords.
+          </div>
+          <div className={isEmailError ? 'alert-shown' : 'alert-hidden'}>
+            This email has already been registered.
+            <br />
+            Please use another email or back to &nbsp;
+            <a
+              href="/login"
+              style={{
+                color: '#3D808E',
+                fontWeight: '500',
+              }}
+            >
+              login
+            </a>
+            .
           </div>
         </div>
       </div>
